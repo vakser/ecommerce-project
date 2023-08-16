3604274790bcf68a1c33a4bcef9ddd399e4ba35a
vakser <65089446+vakser@users.noreply.github.com>
2023-08-16 17:34:28 +0300

    Product list component in Angular app updated

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductService} from "../../services/product.service";
+import {Product} from "../../common/product";
 
 @Component({
   selector: 'app-product-list',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-
-  constructor() { }
+  products: Product[] = [];
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.listProducts();
   }
 
+  private listProducts() {
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
 }
