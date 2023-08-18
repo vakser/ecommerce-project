f16cf5584ac99b979a9020589799c7ddd9dfa152
vakser <65089446+vakser@users.noreply.github.com>
2023-08-18 09:01:42 +0300

    Product list component enhanced to read category id param

--- angular-ecommerce/src/app/services/product.service.ts
+++ angular-ecommerce/src/app/services/product.service.ts
@@ -11,7 +11,7 @@ export class ProductService {
   private baseUrl = 'http://localhost:8080/api/products';
   constructor(private httpClient: HttpClient) { }
 
-  getProductList(): Observable<Product[]> {
+  getProductList(currentCategoryId: number): Observable<Product[]> {
     return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
       map(response => response._embedded.products)
     );
