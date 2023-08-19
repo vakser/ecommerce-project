8a45b3791afac095a0f385f58552ac84feb1985a
vakser <65089446+vakser@users.noreply.github.com>
2023-08-19 18:51:53 +0300

    Angular product service updated to call URL on Spring Boot app

--- angular-ecommerce/src/app/services/product.service.ts
+++ angular-ecommerce/src/app/services/product.service.ts
@@ -16,14 +16,24 @@ export class ProductService {
   getProductList(theCategoryId: number): Observable<Product[]> {
     // build URL based on category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
-    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
-      map(response => response._embedded.products)
-    );
+    return this.getProducts(searchUrl);
+  }
+
+  searchProducts(theKeyword: string): Observable<Product[]> {
+    // build URL based on keyword
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+    return this.getProducts(searchUrl);
   }
 
   getProductCategories(): Observable<ProductCategory[]> {
     return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
       map(response => response._embedded.productCategory)
+    );
+  }
+
+  private getProducts(searchUrl: string) {
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
+      map(response => response._embedded.products)
     );
   }
 }
