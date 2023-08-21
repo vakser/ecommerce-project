eb36e8f56743a76fb1e7e0f6dab41fb19a1a4693
vakser <65089446+vakser@users.noreply.github.com>
2023-08-21 11:00:12 +0300

    Change page-size functionality added to Angular product list component

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -16,7 +16,7 @@ export class ProductListComponent implements OnInit {
 
   // new properties for pagination
   thePageNumber: number = 1;
-  thePageSize: number = 10;
+  thePageSize: number = 5;
   theTotalElements: number = 0;
 
   constructor(private productService: ProductService,
@@ -77,5 +77,11 @@ export class ProductListComponent implements OnInit {
         this.products = data;
       }
     );
+  }
+
+  updatePageSize(pageSize: string) {
+    this.thePageSize = +pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
   }
 }
