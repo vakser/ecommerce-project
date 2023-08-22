44762aaffd8cd044d37671fc458d439bd5dc69fc
vakser <65089446+vakser@users.noreply.github.com>
2023-08-22 07:37:55 +0300

    Click handler for "Add to cart" button added, Angular product list component updated with click handler method.

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -92,4 +92,8 @@ export class ProductListComponent implements OnInit {
       this.theTotalElements = data.page.totalElements;
     };
   }
+
+  addToCart(theProduct: Product) {
+    console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`);
+  }
 }
