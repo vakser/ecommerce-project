b9c5fd85ba977133ef590a93fc1a91f156f72d87
vakser <65089446+vakser@users.noreply.github.com>
2023-08-22 13:36:17 +0300

    Cart service developed for Angular project.

---
+++ angular-ecommerce/src/app/services/cart.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import {CartItem} from "../common/cart-item";
+import {Subject} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  cartItems: CartItem[] = [];
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
+  constructor() { }
+
+  addToCart(theCartItem: CartItem) {
+    // check if we already have the item in out cart
+    let alreadyExistsInCart: boolean = false;
+    let existingCartItem: CartItem = undefined;
+    if (this.cartItems.length > 0) {
+      // find the item in the cart based on item id
+      for (let tempCartItem of this.cartItems) {
+        if (tempCartItem.id === theCartItem.id) {
+          existingCartItem = tempCartItem;
+          break;
+        }
+      }
+      // check if we found the item
+      alreadyExistsInCart = (existingCartItem != undefined);
+    }
+    if (alreadyExistsInCart) {
+      // increment the quantity
+      existingCartItem.quantity++;
+    } else {
+      // just add the item to the array
+      this.cartItems.push(theCartItem);
+    }
+    // compute cart total price and total quantity
+    this.computeCartTotals();
+  }
+
+  private computeCartTotals() {
+
+  }
+}
