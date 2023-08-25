9b4350ca74f4b1294c10a5f8b837ffef447fbd66
vakser <65089446+vakser@users.noreply.github.com>
2023-08-25 18:47:45 +0300

    Methods for getting countries and states added to Angular shop form service.

--- angular-ecommerce/src/app/services/shop-form.service.ts
+++ angular-ecommerce/src/app/services/shop-form.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import {Observable, of} from "rxjs";
 import {HttpClient} from "@angular/common/http";
+import {Country} from "../common/country";
+import {State} from "../common/state";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +13,20 @@ export class ShopFormService {
   private statesUrl = 'http://localhost:8080/api/states';
 
   constructor(private httpClient: HttpClient) { }
+
+  getCountries(): Observable<Country[]> {
+    return this.httpClient.get<GetResponseCountries>(this.countriesUrl).pipe(
+      map(response => response._embedded.countries)
+    );
+  }
+
+  getStates(theCountryCode: string): Observable<State[]> {
+    // search url
+    const searchStatesUrl = `${this.statesUrl}/search/findByCountryCode?code=${theCountryCode}`;
+    return this.httpClient.get<GetResponseStates>(searchStatesUrl).pipe(
+      map(response => response._embedded.states)
+    );
+  }
 
   getCreditCardMonths(startMonth: number): Observable<number[]> {
     let data: number[] = [];
@@ -31,5 +48,18 @@ export class ShopFormService {
       data.push(theYear);
     }
     return of(data);
+  }
+
+}
+
+interface GetResponseCountries {
+  _embedded: {
+    countries: Country[];
+  }
+}
+
+interface GetResponseStates {
+  _embedded: {
+    states: State[];
   }
 }
