ce401a5ce0861e4f7f8f907b6b94a9f7323254ae
vakser <65089446+vakser@users.noreply.github.com>
2023-09-01 08:30:52 +0300

    Spring Boot project configuration for REST controller created.

--- spring-boot-ecommerce/src/main/java/com/ecommerce/controller/CheckoutController.java
+++ spring-boot-ecommerce/src/main/java/com/ecommerce/controller/CheckoutController.java
@@ -5,7 +5,6 @@ import com.ecommerce.dto.PurchaseResponse;
 import com.ecommerce.service.CheckoutService;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
