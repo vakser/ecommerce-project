c2fe7731ba1875d26989634621ada33d88157bcc
vakser <65089446+vakser@users.noreply.github.com>
2023-09-02 13:18:29 +0300

    Login status component of Angular project updated to keep track of logged-in user's email with web browser storage. Class OrderHistory created.

--- angular-ecommerce/src/app/components/login-status/login-status.component.ts
+++ angular-ecommerce/src/app/components/login-status/login-status.component.ts
@@ -10,6 +10,7 @@ import {OktaAuth} from "@okta/okta-auth-js";
 export class LoginStatusComponent implements OnInit {
   isAuthenticated: boolean = false;
   userFullName: string = '';
+  storage: Storage = sessionStorage;
 
   constructor(private oktaAuthService: OktaAuthStateService, @Inject(OKTA_AUTH) private oktaAuth: OktaAuth) { }
 
@@ -30,6 +31,10 @@ export class LoginStatusComponent implements OnInit {
       this.oktaAuth.getUser().then(
         (res) => {
           this.userFullName = res.name as string;
+          // retrieve the user's email from authentication response
+          const theEmail = res.email;
+          // store the email in browser storage
+          this.storage.setItem('userEmail', JSON.stringify(theEmail));
         }
       );
     }
