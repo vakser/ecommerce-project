6aa03c33470d340f8f33c1fa7d4618e12903032e
vakser <65089446+vakser@users.noreply.github.com>
2023-09-04 07:25:32 +0300

    SpringSecurity configuration class added to protect endpoints for authenticated users.

--- spring-boot-ecommerce/src/main/java/com/ecommerce/config/SecurityConfiguration.java
+++ spring-boot-ecommerce/src/main/java/com/ecommerce/config/SecurityConfiguration.java
@@ -1,9 +1,12 @@
 package com.ecommerce.config;
 
+import com.okta.spring.boot.oauth.Okta;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
 
 @Configuration
 public class SecurityConfiguration {
@@ -15,7 +18,12 @@ public class SecurityConfiguration {
                         .authenticated())
                 .oauth2ResourceServer()
                 .jwt();
+        // add CORS filters
         http.cors();
+        // add content negotiation strategy
+        http.setSharedObject(ContentNegotiationStrategy.class, new HeaderContentNegotiationStrategy());
+        // force a non-empty response body for 401 to make the response more friendly
+        Okta.configureResourceServer401ResponseBody(http);
         return http.build();
     }
 }
