01dd09287766b95493462e88a86916155469ec26
vakser <65089446+vakser@users.noreply.github.com>
2023-09-06 08:38:19 +0300

    CheckoutController updated to expose /payment-intent endpoint.

--- spring-boot-ecommerce/src/main/java/com/ecommerce/controller/CheckoutController.java
+++ spring-boot-ecommerce/src/main/java/com/ecommerce/controller/CheckoutController.java
@@ -1,8 +1,13 @@
 package com.ecommerce.controller;
 
+import com.ecommerce.dto.PaymentInfo;
 import com.ecommerce.dto.Purchase;
 import com.ecommerce.dto.PurchaseResponse;
 import com.ecommerce.service.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,5 +23,12 @@ public class CheckoutController {
     public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
         PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
         return purchaseResponse;
+    }
+
+    @PostMapping("payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+        PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+        String paymentStr = paymentIntent.toJson();
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
     }
 }
