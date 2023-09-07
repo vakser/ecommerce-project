e6b1d4351345f9680c0f6085d63ebdd54e54f686
vakser <65089446+vakser@users.noreply.github.com>
2023-09-07 18:46:34 +0300

    Checkout component of Angular project updated to disable "Purchase" button after it was clicked once.

--- angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -33,6 +33,8 @@ export class CheckoutComponent implements OnInit {
   cardElement: any;
   displayError: any = "";
 
+  isDisabled: boolean = false;
+
   constructor(private formBuilder: FormBuilder,
               private shopFormService: ShopFormService,
               private cartService: CartService,
@@ -122,6 +124,7 @@ export class CheckoutComponent implements OnInit {
     // - confirm card payment
     // - place order
     if (!this.checkoutFormGroup.invalid && this.displayError.textContent === "") {
+      this.isDisabled = true;
       this.checkoutService.createPaymentIntent(this.paymentInfo).subscribe((paymentIntentResponse) => {
         this.stripe.confirmCardPayment(paymentIntentResponse.client_secret,
           {
@@ -146,6 +149,7 @@ export class CheckoutComponent implements OnInit {
             if (result.error) {
               // inform the customer there was an error
               alert(`There was an error: ${result.error.message}`);
+              this.isDisabled = false;
             } else {
               // call REST API via the CheckoutService
               this.checkoutService.placeOrder(purchase).subscribe({
@@ -153,9 +157,11 @@ export class CheckoutComponent implements OnInit {
                   alert(`Your order has been received.\nOrder tracking number: ${response.orderTrackingNumber}`);
                   // reset cart
                   this.resetCart();
+                  this.isDisabled = false;
                 },
                 error: (err: any) => {
                   alert(`There was an error: ${err.message}`);
+                  this.isDisabled = false;
                 }
               })
             }
